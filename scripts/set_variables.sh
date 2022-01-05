a52dc2b66690efb943b07b6bd06f9a86e21721e8
Tarlan Huseynov <tarlan.huseynov@rabita.az>
2022-01-05 22:40:09 +0400

    new release

--- scripts/set_variables.sh
+++ scripts/set_variables.sh
@@ -5,8 +5,8 @@ ansible_host: "{{ lookup('env','IIS_SERVER') }}"
 ansible_winrm_scheme: http
 ansible_port: 5985
 ansible_connection: winrm
-ansible_user: "{{ lookup('env','ANSIBLE_USER') }}"
-ansible_password: "{{ lookup('env','ANSIBLE_PASS') }}"
+ansible_user: "{{ lookup('community.hashi_vault.hashi_vault', 'secret=iis/{{ ansible_host }}:username token=s.O6kDO74PBM4eiEm6TClmf9gX url=http://10.10.15.57:8200') }}"
+ansible_password: "{{ lookup('community.hashi_vault.hashi_vault', 'secret=iis/{{ ansible_host }}:password token=s.O6kDO74PBM4eiEm6TClmf9gX url=http://10.10.15.57:8200') }}"
 ansible_winrm_transport: ntlm
 EOF
 
